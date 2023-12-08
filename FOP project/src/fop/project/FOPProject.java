4609315d3e11f53c06c6a29c47c76d782caa3183
SarahAl-Maleki <23061281@siswa.um.edu.my>
2023-12-08 10:52:09 +0800

    how are you

--- FOP project/src/fop/project/FOPProject.java
+++ FOP project/src/fop/project/FOPProject.java
@@ -15,6 +15,8 @@ public class FOPProject {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println("how are you")
     }
     
 }
