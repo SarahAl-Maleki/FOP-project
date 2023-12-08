59c2f95a5109bb986af49ec8f930d5116d3f80ab
ririgwanghae <23004880@siswa.um.edu.my>
2023-12-08 10:58:08 +0800

    test

--- FOP project/src/fop/project/FOPProject.java
+++ FOP project/src/fop/project/FOPProject.java
@@ -15,6 +15,8 @@ public class FOPProject {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println("My name is riri");
     }
     
 }
